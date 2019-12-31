79a727f5d5d13a0ec2313e1a61eeed9cc24f8f36
Dipendra <dipu.danish@outlook.com>
2019-12-31 06:35:48 +0545

    2nd commit

--- iit/settings.py
+++ iit/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'iit.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
